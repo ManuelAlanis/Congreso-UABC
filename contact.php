800365c0738f5953a2ee3c427bb96cdbfb7e0e21
manuelalanis <alanis@uabc.edu.mx>
2014-11-29 12:33:07 -0800

    cogreso

--- contact.php
+++ contact.php
@@ -1,18 +1,18 @@
 <?php
 
-$EmailFrom = $_REQUEST['email'];
-$EmailTo = "man.alaniz@gmail.com"; // Your email address here XD
+$EmailFrom = $_REQUEST['email']; 
+$EmailTo = "info@byronwd.com"; // Your email address here
 $Subject = "Contact form";
-$Name = Trim(stripslashes($_POST['name']));
-$Email = Trim(stripslashes($_POST['email']));
-$Message = Trim(stripslashes($_POST['message']));
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
 
 // validation
-// $validationOK=true;
-// if (!$validationOK) {
-//   echo "Error";
-//   exit;
-// }
+$validationOK=true;
+if (!$validationOK) {
+  echo "Error";
+  exit;
+}
 
 // prepare email body text
 $Body = "";
@@ -27,18 +27,16 @@ $Body .= "\n";
 $Body .= "\n";
 $Body .= $Message;
 $Body .= "\n";
-// send email
 
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 
-// redirect to success page
+// redirect to success page 
 if ($success){
-
-
- header("Location: http://byronwd.com/contact.html");
-  // echo "Succes";
+ header("Location: http://107.170.205.133/contact.html");
+  echo "Succes";
 }
 else{
-
   echo "Error";
 }
 ?>

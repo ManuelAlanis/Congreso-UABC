e665755090ab1979b52f1bbfd0328c6b195e5ff4
manuelalanis <alanis@uabc.edu.mx>
2014-11-29 13:06:22 -0800

    cogreso

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 
 $EmailFrom = $_REQUEST['email']; 
-$EmailTo = "info@byronwd.com"; // Your email address here
+$EmailTo = "man.alaniz@byronwd.com"; // Your email address here
 $Subject = "Contact form";
 $Name = Trim(stripslashes($_POST['name'])); 
 $Email = Trim(stripslashes($_POST['email'])); 
